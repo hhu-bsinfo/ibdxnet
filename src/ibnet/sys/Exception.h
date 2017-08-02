09c0fa6cec504c7d9e878e1b35485226924611df
Stefan Nothaas <stefan.nothaas@hhu.de>
2017-08-02 13:35:00 +0200

    sys: Minor cleanup, add copyright header

--- src/ibnet/sys/Exception.h
+++ src/ibnet/sys/Exception.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2017 Heinrich-Heine-Universitaet Duesseldorf,
+ * Institute of Computer Science, Department Operating Systems
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 #ifndef IBNET_SYS_EXCEPTION_H
 #define IBNET_SYS_EXCEPTION_H
 
@@ -13,7 +31,7 @@ namespace ibnet {
 namespace sys {
 
 /**
- * Exception base class. Use this for all exceptions
+ * Exception base class
  *
  * @author Stefan Nothaas, stefan.nothaas@hhu.de, 01.06.2017
  */

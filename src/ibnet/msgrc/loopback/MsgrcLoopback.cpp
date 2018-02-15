503c44efdfbe1d1e01b9d92e34c216ce6aa190fb
Stefan Nothaas <stefan.nothaas@hhu.de>
2018-02-15 14:44:44 +0100

    MsgrcLoopback: Add signal handler to shut down on sigint

--- src/ibnet/msgrc/loopback/MsgrcLoopback.cpp
+++ src/ibnet/msgrc/loopback/MsgrcLoopback.cpp
@@ -3,13 +3,24 @@
 
 #include "MsgrcLoopbackSystem.h"
 
+static bool g_loop = true;
+
+static void SignalHandler(int signal)
+{
+    if (signal == SIGINT) {
+        g_loop = false;
+    }
+}
+
 int main(int argc, char** argv)
 {
     auto* loopback = new ibnet::msgrc::MsgrcLoopbackSystem(argc, argv);
 
     loopback->Init();
 
-    while (true) {
+    signal(SIGINT, SignalHandler);
+
+    while (g_loop) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 

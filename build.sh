6fb1397593532e31ffd4af7a66ef94508d742bd5
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2018-09-27 13:24:23 +0200

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+build_type="release"
+
+if [ "$1" ]; then
+    build_type="$1"
+fi
+
+FLAGS=""
+
+case "$build_type" in
+    debug)
+        ;;
+    release)
+        FLAGS="IBNET_DISABLE_STATISTICS"
+        ;;
+    *)
+        echo "Invalid build type \"$build_type\" specified"
+        exit -1
+esac
+
+echo "Build type: $build_type"
+
 mkdir -p build
 cd build
 
-COMPILER_FLAGS=""
-
-for flag in "$@"; do
+for flag in "$FLAGS"; do
     COMPILER_FLAGS="$COMPILER_FLAGS -D$flag"
 done
 
